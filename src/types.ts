af57fc5c9e882fa5196b0263f526c2e03e5c8282
Vishant V <vadhervishant@gmail.com>
2025-08-25 01:07:33 -0400

    Added the types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,24 @@
-export type Attributes = {
-    Strength: number;
-    Dexterity: number;
-    Constitution: number;
-    Intelligence: number;
-    Wisdom: number;
-    Charisma: number;
-};
+export type AttributeType = 'Strength' | 'Dexterity' | 'Constitution' | 'Intelligence' | 'Wisdom' | 'Charisma';
+export type Class = 'Barbarian' | 'Wizard' | 'Bard';
 
-export type Class = "Barbarian" | "Wizard" | "Bard";
+export interface Attributes {
+  Strength: number;
+  Dexterity: number;
+  Constitution: number;
+  Intelligence: number;
+  Wisdom: number;
+  Charisma: number;
+}
+
+export interface Skill {
+  name: string;
+  attributeModifier: AttributeType;
+  points: number;
+}
+
+export interface Character {
+  id: string;
+  name: string;
+  attributes: Attributes;
+  skills: Skill[];
+}
